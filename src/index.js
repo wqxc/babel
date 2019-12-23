f596262b539f1f34eba32a03e5fac1050cfd4477
edz <edz@edzdeMacBook-Air-3.local>
2019-12-23 11:06:08 +0800

    feat: creare

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import * as recast from "recast";
+import React from "react";
+class App extends React.Component {
+  componentDidMount() {
+    console.log("123");
+  }
+  render() {
+    return <div>1233</div>;
+  }
+}
